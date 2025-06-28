a6d975219c37f1d26cfea92ca29e4f1a2f07b26b
Mario <mario@gmail.com>
2025-06-28 17:03:44 +0300

    Add project files.

---
+++ MouseHookLib/MouseHook.cs
@@ -0,0 +1,49 @@
+﻿using EasyHook;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace MouseHookLib
+{
+    public class MouseHook
+    {
+        private LocalHook _hook;
+
+        //?
+        public MouseHook(RemoteHooking.IContext context) { }
+
+        public void Run(RemoteHooking.IContext context)
+        {
+            try
+            {
+                _hook = LocalHook.Create(
+                        LocalHook.GetProcAddress("user32.dll", "SetCursorPos"),
+                        new SetCursorPosDelegate(SetCursorPos_Hooked),
+                        this
+                    );
+                //Init thread for hook
+                _hook.ThreadACL.SetInclusiveACL(new int[] { 0 });
+
+                while (true)
+                {
+                    Thread.Sleep(500);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Hook error: {ex.Message}");
+            }
+        }
+
+        private bool SetCursorPos_Hooked(int x, int y)
+        {
+            Console.WriteLine($"Mouse moved to: {x}, {y}");
+            return SetCursorPos(x, y);
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, SetLastError = true)]
+        delegate bool SetCursorPosDelegate(int x, int y);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetCursorPos(int X, int Y);
+    }
+}
---
+++ hooks_app/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace hooks_app
+{
+    public class Program
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetCursorPos(int X, int Y);
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                SetCursorPos(100 + i * 10, 100 + i * 10);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+    }
+}
